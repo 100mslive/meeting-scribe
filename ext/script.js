3f59d4b728a31beb14b2e8bab65006c3a10be035
amar-1995 <amar@100ms.live>
2024-01-08 04:15:36 +0530

    feat: initial cleanup

--- ext/script.js
+++ ext/script.js
@@ -1,29 +1,22 @@
 chrome.runtime.onMessageExternal.addListener(async function (message, sender, sendResponse) {
   if (message.action === "uploadFile" && message.data) {
-    // filename = `stream_${message.streamId}/track_${message.trackId}/${message.kind}/${Date.now()}.webm`
-    filename = `stream_data/track_data/${message.kind}/${Date.now()}.webm`
+    const streamId =  message.streamId;
+    const trackId = message.trackId;
+    const kind = message.kind
+    filename = `${streamId ? streamId : 'stream_data'}/${trackId}/${kind}/${Date.now()}.webm`
     chrome.downloads.download(options={
       url: message.data,
       filename,
       saveAs: false,
       conflictAction: "uniquify",
-    })
+    });
   }
   const data = message.data;
-  // if (message[0] !== 'WebRTCExternals') return;
+  if (message.action !== "log") return;
   const p = document.createElement("p");
   p.append(data);
   const logDiv = document.getElementById("log");
   logDiv.appendChild(p);
 });
 
-// chrome.storage.local.get(['uploadedBlob'], function(result) {
-//   if (!result) return;
-//   console.log("blob ", result);
-// });
 console.log("loaded");
-
-/**.
-  
-
- */
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer-extra";
 import { Command } from "commander";
+import UserPreferencesPlugin from "puppeteer-extra-plugin-user-preferences";
 
 import fs from 'fs';
 import path from 'path';
 import chokidar from 'chokidar';
 
+const manageAVFilesName = {};
+const DOWNLOAD_DIRECTORY = '/Users/amar_1995/myAwesomeDownloadFolder';
 function concatenateFilesInDirectory(directoryPath, targetFilePath) {
     const directoryCheckInterval = setInterval(() => {
       if (fs.existsSync(directoryPath)) {
@@ -50,7 +53,25 @@ function appendFileContent(sourceFilePath, targetFilePath) {
     const content = fs.readFileSync(sourceFilePath);
     fs.appendFileSync(targetFilePath, content);
 }
+// wait in millisec
+function wait(time = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+}
 
+function downloadUserLocation() {
+  return UserPreferencesPlugin({
+    userPrefs: {
+      download: {
+        prompt_for_download: false,
+        default_directory: DOWNLOAD_DIRECTORY,
+      }
+    },
+  });
+}
 (async () => {
   const program = new Command();
   program
@@ -58,7 +79,7 @@ function appendFileContent(sourceFilePath, targetFilePath) {
     .description('Record audio and video tracks of each peer in a meeting');
 
   program
-    .requiredOption('-u, --url <meeting_url>', 'meeting url to join')
+    .requiredOption('-u, --url <meeting_url>', 'meeting url to join') 
     .option('-o, --output_dir <dir>', 'directory to store recordings', process.cwd())
 
   program.parse()
@@ -66,6 +87,7 @@ function appendFileContent(sourceFilePath, targetFilePath) {
   const meetingLink = options.url;
   const outputDir = options.output_dir;
 
+  puppeteer.use(downloadUserLocation());
   const browser = await puppeteer.launch({
     executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
     headless: false,
@@ -76,45 +98,60 @@ function appendFileContent(sourceFilePath, targetFilePath) {
       '--start-maximized',
       '--no-default-browser-check',
       '--auto-accept-camera-and-microphone-capture',
-      '--load-extension=ext'
+      '--load-extension=ext',
     ]
-  })
-  const page = await browser.pages().then(e => e[0]);
-
-  await page.goto(meetingLink)
-  await page.locator("input").fill("bot")
-  const joinButton = await page.waitForSelector(
-    'div >>> ::-p-text("Join")'
-  )
-
+  });
+  // wait for browser to load
+  await wait(2000);
   // Find the extension
-  const targets = await browser.targets();
+  const targets = browser.targets();
   const extensionTarget = targets.find(target => {
     return target.type() === "service_worker"
   });
-
   // Extract the URL
   const extensionURL = extensionTarget.url();
   const urlSplit = extensionURL.split('/');
   const extensionID = urlSplit[2];
-
   // Define the extension page
   const extensionEndURL = 'index.html';
-
-  //Navigate to the page
+  // const extPage = await browser.pages().then(e => e[0]);
   const extPage = await browser.newPage();
+  //Navigate to the page
   await extPage.goto(`chrome-extension://${extensionID}/${extensionEndURL}`);
 
+  // load 100ms url
+  const page = await browser.newPage();
   await page.bringToFront();
+  await page.exposeFunction("__100ms_getExtensionId", () => {
+    return extensionID;
+  });
+  await page.goto(meetingLink);
+  // js me global variable ---- 
+  await page.locator("input").fill("bot");
+  const joinButton = await page.waitForSelector('div >>> ::-p-text("Join")');
   await joinButton.click();
-
   if (!fs.existsSync(outputDir)){
-      fs.mkdirSync(outputDir);
-  }
+    fs.mkdirSync(outputDir);
+  } 
+  await page.exposeFunction('__100ms_onMessageReceivedEvent', data  => {
+    try {
+      data = JSON.parse(data);
+    } catch(e) {
 
-  const audioFilePath = path.join(outputDir, 'audio.webm')
-  const videoFilePath = path.join(outputDir, 'video.webm')
-
-  concatenateFilesInDirectory("/Users/shubham/Downloads/stream_data/track_data/audio/", audioFilePath);
-  concatenateFilesInDirectory("/Users/shubham/Downloads/stream_data/track_data/video/", videoFilePath);
+    }
+    if (!data.streamId) {
+      // if no streamId sent by the vendor
+      data.streamId = 'stream_data';
+    }
+    if (!manageAVFilesName[data.trackId] && data.action === 'AVTracksAdded') {
+      manageAVFilesName[data.trackId] = {kind: data.kind, streamId: data.streamId};
+      const downloadFileDirPath = path.join(outputDir, data.trackId);
+      if (!fs.existsSync(downloadFileDirPath)) {
+        fs.mkdirSync(downloadFileDirPath);
+      }
+      const downloadFilePath = path.join(downloadFileDirPath, `${data.kind}.webm`);
+      const trackFilePath = path.join(DOWNLOAD_DIRECTORY, data.streamId, data.trackId, data.kind);
+      concatenateFilesInDirectory(trackFilePath, downloadFilePath);
+    }
+  });
 })();
